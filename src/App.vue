da1577351aa6d93e66589fddac7ebd10b8f5f02f
Nazar <nazarkosandyak@gmail.com>
2022-10-02 13:13:48 +0300

    add about section

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,18 @@
     <!-- <h2>Deployment</h2>   -->
     <h1>Main App</h1>
     <div>
+      <About />
       <!-- <Messaging /> -->
     </div>
   </div>
 </template>
 
 <script>
+import About from './components/about/About.vue';
 import Messaging from './components/messaging/Messaging.vue'
 export default {
   components: {
+    About,
     Messaging
   },
   setup() {
